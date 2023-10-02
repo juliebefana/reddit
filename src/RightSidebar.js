2660e44c46fb89915187f9b72883b6b4c1cc783e
juliebefana <92132933+juliebefana@users.noreply.github.com>
2023-10-02 12:13:06 +0000

    Styled right sidebar

--- src/RightSidebar.js
+++ src/RightSidebar.js
@@ -1,32 +1,28 @@
-import React from 'react'
-import './RightSidebar.css'
+import React from 'react';
+import './RightSidebar.css';
 
-export default function RightSidar() {
-    const menus = [
-        { to: '/r/explainlikeimfive', text: "r/explainlikeimfive"},
-        { to: '/r/explainlikeimfive', text: "r/IAmA"},
-        { to: '/r/classicwow', text: "r/classicwow"},
-        { to: '/r/Instagram', text: "r/Instagram"},
-        { to: '/r/NintendoSwitch', text: "r/NintendoSwitch"},
-        
-    ]
-    return (
-        <div className="RightSidebar">
-            
-                <ul 
-                className="RightSidebar__menu">
-                    <h1 
-                    className="popular_com">Popular communities</h1>
-                    <hr></hr>
-                    {menus.map(menu => (
-                        <li>
-                            <a className="topics" href={menu.to}>{menu.text}</a>
-                            </li>
-                    ))}
-                </ul>
-                
-                
-            
-        </div>
-    )
+export default function RightSidebar() {
+  const communities = [
+    { name: 'r/explainlikeimfive', members: Math.floor(Math.random() * 10000) },
+    { name: 'r/IAmA', members: Math.floor(Math.random() * 10000) },
+    { name: 'r/classicwow', members: Math.floor(Math.random() * 10000) },
+    { name: 'r/Instagram', members: Math.floor(Math.random() * 10000) },
+    { name: 'r/NintendoSwitch', members: Math.floor(Math.random() * 10000) },
+  ];
+
+  return (
+    <div className="RightSidebar">
+      <ul className="RightSidebar__menu">
+        <h2 className="popular_com">Popular communities</h2>
+        <hr />
+        {communities.map((community, index) => (
+          <li key={index} className="community-item">
+            <span className="community-name">{community.name}</span>
+            <span className="member-count">{`${community.members} members`}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
